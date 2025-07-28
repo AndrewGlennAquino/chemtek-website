b49773371b5d431b8fae2dccf1ca32c392e0ed61
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-28 18:57:23 -0500

    Create header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,90 @@
+import HamburgerMenu from "../hamburger-menu/HamburgerMenu";
+import { motion, type Variants } from "motion/react";
+import { Link } from "react-router";
+
+/**
+ * Header component that contains logo and responsive nav
+ */
+const Header = () => {
+  // Animation variants to animate links on hover
+  const variants: Variants = {
+    initial: {
+      width: 0,
+    },
+    animateHover: {
+      width: "100%",
+    },
+  };
+
+  return (
+    <header className="bg-smoke shadow fixed top-0 left-0 right-0">
+      {/* Header container */}
+      <div className="container w-full h-16 mp-default flex justify-between items-center">
+        {/* Header logo */}
+        <Link to="/chemtek-website">
+          <h1 className="text-chemtek text-3xl font-bold">ChemTek</h1>
+        </Link>
+
+        {/* Responsive navigation menu */}
+        <nav>
+          {/* Hamburger menu only appears for device width less than 1280px */}
+          <HamburgerMenu />
+
+          {/* Links container for device width 1280px and greater */}
+          <div className="hidden xl:flex justify-center items-center gap-8">
+            <motion.span
+              className="dropdown-link"
+              initial="initial"
+              whileHover="animateHover"
+            >
+              <Link to="">Home</Link>
+              <motion.div className="link-underline" variants={variants} />
+            </motion.span>
+
+            <motion.span
+              className="dropdown-link"
+              initial="initial"
+              whileHover="animateHover"
+            >
+              <Link to="">Why Choose Us</Link>
+              <motion.div className="link-underline" variants={variants} />
+            </motion.span>
+
+            <motion.span
+              className="dropdown-link"
+              initial="initial"
+              whileHover="animateHover"
+            >
+              <Link to="">Service Area</Link>
+              <motion.div className="link-underline" variants={variants} />
+            </motion.span>
+
+            <motion.span
+              className="dropdown-link"
+              initial="initial"
+              whileHover="animateHover"
+            >
+              <a
+                href="https://financing.approvepayments.com/chemtek"
+                target="_blank"
+              >
+                Equipment Lease/Finance
+              </a>
+              <motion.div className="link-underline" variants={variants} />
+            </motion.span>
+
+            <motion.span
+              className="dropdown-link contact-us-button"
+              whileHover={{ scale: 1.1 }}
+              variants={variants}
+            >
+              <Link to="">Contact Us</Link>
+            </motion.span>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
