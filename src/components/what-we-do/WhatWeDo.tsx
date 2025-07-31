f4fb84b7cf5160bc5daa50cca417d7d163e55eba
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-30 19:59:50 -0500

    Create What We Do section with cards

---
+++ src/components/what-we-do/WhatWeDo.tsx
@@ -0,0 +1,46 @@
+import { motion, useReducedMotion, MotionConfig } from "motion/react";
+import Card from "../card/Card";
+import cleanImage from "../../assets/images/clean.png";
+import iceImage from "../../assets/images/ice.png";
+import washImage from "../../assets/images/wash.png";
+
+/**
+ * What We Do component for Home page
+ */
+const WhatWeDo = () => {
+  return (
+    <section aria-label="What we do">
+      {/* What We Do container */}
+      <div className="container mp-default flex flex-col gap-4">
+        {/* What We Do Header */}
+        <h1 className="text-3xl font-bold">What We Do</h1>
+
+        {/* Card container */}
+        <div className="grid grid-rows-3 lg:grid-rows-1 lg:grid-cols-3 gap-4 lg:gap-8">
+          <Card
+            src={cleanImage}
+            alt="Hand wiping with towel"
+            title="Cleaning"
+            description="We provide low cost cleaning products and services to help keep your facility clean and expenses to a minimum"
+          />
+
+          <Card
+            src={iceImage}
+            alt="Ice machine"
+            title="Ice Machines"
+            description="Our reliable and prompt service will satisfy your need for a clean and working ice machine"
+          />
+
+          <Card
+            src={washImage}
+            alt="Clean plate"
+            title="Cleaning"
+            description="Our team of experts can guarantee proper dish washer maintenance, ensuring hygienic warewashing results"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WhatWeDo;
