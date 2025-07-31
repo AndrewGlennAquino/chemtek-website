51d1347b58416e27c0f8517f77700c2a7e377b8f
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-30 19:58:52 -0500

    Add Footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="bg-chemtek text-smoke text-sm">
+      {/* Footer container */}
+      <div className="container mp-default">
+      <a href="https://www.flaticon.com/" title="Icons">Icons created by Freepik - Flaticon</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
