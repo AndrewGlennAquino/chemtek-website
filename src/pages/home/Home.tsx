4ad0cc33bc47c3ff839faf77750cae052c814c6a
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-31 20:48:33 -0500

    Add testimonial section to Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from "../../components/hero/Hero";
 import WhatWeDo from "../../components/what-we-do/WhatWeDo";
+import Testimonials from "../../components/testimonials/Testimonials";
 
 const Home = () => {
   return (
     <main>
       <Hero />
       <WhatWeDo />
+      <Testimonials />
     </main>
   );
 };
