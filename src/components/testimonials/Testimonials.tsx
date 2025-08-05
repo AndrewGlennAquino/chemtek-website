f5c242b2219bce33bdaaa34e0ac8d98ec1fb8a91
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-08-04 19:39:07 -0500

    Hide quotation aria-label; Add strong tags around words of emphasis

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -6,26 +6,26 @@ interface TestimonialProps {
   src: string;
   author: string;
   company: string;
-  desc: string;
+  children: React.ReactElement | string;
 }
 
 /**
  * Testimonial component that takes an
- * image source, author, company, and description
+ * image source, author, company, and children
  *
- * @param props image source, author, company, desc
+ * @param props image source, author, company, children
  */
-const Testimonial = ({ src, author, company, desc }: TestimonialProps) => {
+const Testimonial = ({ src, author, company, children }: TestimonialProps) => {
   return (
-    <div
+    <article
       aria-label={`Testimonial from ${author} on FaceBook`}
       className="bg-smoke card-shadow w-full h-full p-6 rounded-4xl flex flex-col gap-4"
     >
       {/* Testimonial description */}
       <div>
-        <div className="testimonial-quote rotate-180 relative -left-2">"</div>
-        <p className="lg:px-16">{desc}</p>
-        <div className="testimonial-quote ml-auto relative -right-2">"</div>
+        <div aria-hidden={true} className="testimonial-quote rotate-180 relative -left-2">"</div>
+        <p className="lg:px-16">{children}</p>
+        <div aria-hidden={true} className="testimonial-quote ml-auto relative -right-2">"</div>
       </div>
 
       {/* Image, author, and company container */}
@@ -47,7 +47,7 @@ const Testimonial = ({ src, author, company, desc }: TestimonialProps) => {
           <h2 className="text-night/60 leading-tight">{company}</h2>
         </div>
       </div>
-    </div>
+    </article>
   );
 };
 
@@ -66,10 +66,27 @@ const Testimonials = () => {
         <div>
           <Testimonial
             src={userIcon}
-            author="Julio Benitez"
+            author="Julio B."
             company="No Manches Mexican Grill"
-            desc="I rarely write reviews, but I felt compelled to share my experience with ChemTek because they truly stand out from the crowd. The quality of their products is consistently excellent, which is so important to me, and their prices are honestly some of the best I’ve found anywhere. It’s clear they care about both their products and their customers. I also want to give a special shoutout to Vince, the owner. He’s one of those rare business owners who genuinely goes above and beyond. He’s always friendly, helpful, and willing to take the time to make sure everything is perfect. It’s obvious he’s passionate about what he does and truly cares about his customers’ satisfaction. If you’re on the fence about choosing ChemTek, I can honestly say you won’t be disappointed. Great products, amazing prices, and the kind of customer service that’s almost impossible to find these days. Highly recommended!"
-          />
+          >
+              <>
+                I rarely write reviews, but I felt compelled to share my
+                experience with ChemTek because they <strong>truly stand out from the
+                crowd</strong>. The quality of their products is consistently excellent,
+                which is so important to me, and their prices are honestly some
+                of the best I’ve found anywhere. <strong>It’s clear they care about both
+                their products and their customers.</strong> I also want to give a
+                special shoutout to Vince, the owner. He’s one of those rare
+                business owners who genuinely goes above and beyond. He’s always
+                friendly, helpful, and willing to take the time to make sure
+                everything is perfect. It’s obvious he’s passionate about what
+                he does and <strong>truly cares about his customers’ satisfaction</strong>. If
+                you’re on the fence about choosing ChemTek, I can honestly say
+                you won’t be disappointed. Great products, amazing prices, and
+                the kind of customer service that’s almost impossible to find
+                these days. Highly recommended!
+              </>
+          </Testimonial>
         </div>
 
         {/* View all reviews button */}
