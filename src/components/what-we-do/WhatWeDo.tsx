e028f574ecd0dbb8e0c0dad09d17ad0b9da82ce4
Andrew Aquino <andrewglenn.aquino@gmail.com>
2025-08-06 17:26:52 -0500

    Animate content fade in on view

--- src/components/what-we-do/WhatWeDo.tsx
+++ src/components/what-we-do/WhatWeDo.tsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 import Card from "../card/Card";
 import cleanImage from "../../assets/images/clean.png";
 import iceImage from "../../assets/images/ice.png";
@@ -15,7 +16,16 @@ const WhatWeDo = () => {
         <h1 className="">What We Do</h1>
 
         {/* Card container */}
-        <div className="grid grid-rows-3 lg:grid-rows-1 lg:grid-cols-3 gap-4 lg:gap-8">
+        <motion.div
+          className="grid grid-rows-3 lg:grid-rows-1 lg:grid-cols-3 gap-4 lg:gap-8"
+          initial={{ y: 20, opacity: 0 }}
+          whileInView={{
+            y: 0,
+            opacity: 1,
+            transition: { delay: 0.5, duration: 0.4, ease: "easeOut" },
+          }}
+          viewport={{ once: true }}
+        >
           <Card
             src={cleanImage}
             alt="Hand wiping with towel"
@@ -36,7 +46,7 @@ const WhatWeDo = () => {
             title="Cleaning"
             description="Our team of experts can guarantee proper dish washer maintenance, ensuring hygienic warewashing results"
           />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
--- src/components/why-choose-us/WhyChooseUs.tsx
+++ src/components/why-choose-us/WhyChooseUs.tsx
@@ -1,3 +1,4 @@
+import { motion, type Variants, stagger } from "motion/react";
 import whyChooseUs from "../../assets/images/why-choose-us.jpg";
 import zap from "../../assets/icons/zap.svg";
 import tool from "../../assets/icons/tool.svg";
@@ -8,14 +9,24 @@ interface WhyChooseUsCardProps {
   src: string;
   title: string;
   desc: string;
+  variants: Variants;
 }
 
-const WhyChooseUsCard = ({ bg, src, title, desc }: WhyChooseUsCardProps) => {
+const WhyChooseUsCard = ({
+  bg,
+  src,
+  title,
+  desc,
+  variants,
+}: WhyChooseUsCardProps) => {
   return (
-    <div className="card-shadow w-full h-full p-4 rounded-lg flex flex-col md:flex-row items-center gap-8">
+    <motion.div
+      className="card-shadow w-full h-full p-4 rounded-lg flex flex-col md:flex-row items-center gap-8"
+      variants={variants}
+    >
       {/* Image with background color */}
       <div className={`${bg} w-fit h-fit p-1 shrink-0 rounded-lg`}>
-        <img src={src} alt={`Image of ${title}`} loading="lazy"/>
+        <img src={src} alt={`Image of ${title}`} loading="lazy" />
       </div>
 
       {/* Card text container */}
@@ -23,11 +34,21 @@ const WhyChooseUsCard = ({ bg, src, title, desc }: WhyChooseUsCardProps) => {
         <h2 className="text-lg font-bold">{title}</h2>
         <p>{desc}</p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
 const WhyChooseUs = () => {
+  // Animation variants for card fade in
+  const fadeInVariants: Variants = {
+    initial: { y: 25, opacity: 0 },
+    animateFadeIn: {
+      y: 0,
+      opacity: 1,
+      transition: { duration: 0.4, ease: "easeOut" },
+    },
+  };
+
   return (
     <section aria-label="Why choose us">
       {/* Why choose us container spacing */}
@@ -40,12 +61,25 @@ const WhyChooseUs = () => {
             <h1>Why Choose Us </h1>
 
             {/* Why choose us grid container */}
-            <div className="grid grid-rows-3 grid-cols-1 gap-4 lg:gap-8">
+            <motion.div
+              className="grid grid-rows-3 grid-cols-1 gap-4 lg:gap-8"
+              initial="initial"
+              whileInView="animateFadeIn"
+              variants={{
+                animateFadeIn: {
+                  transition: {
+                    delayChildren: stagger(0.1, {startDelay: 0.1}),
+                  },
+                },
+              }}
+              viewport={{ amount: 0.25, once: true }}
+            >
               <WhyChooseUsCard
                 bg="bg-yellow-300"
                 src={zap}
                 title="Realiable Products and Services"
                 desc="Our service and products are designed to be energy efficient and reliable, ensuring that your business runs smoothly and efficiently. We use only the highest quality materials and components in our products."
+                variants={fadeInVariants}
               />
 
               <WhyChooseUsCard
@@ -53,14 +87,16 @@ const WhyChooseUs = () => {
                 src={tool}
                 title="Installation and Maintenance"
                 desc="Our team of experienced technicians provides expert installation and maintenance services for all your ice machines and warewashing equipment. We also offer regular maintenance plans to keep your equipment running at peak performance."
+                variants={fadeInVariants}
               />
               <WhyChooseUsCard
                 bg="bg-orange-300"
                 src={users}
                 title="Exceptional Customer Service"
                 desc="We pride ourselves on providing exceptional customer service. Our team is dedicated to ensuring that our customers are satisfied with our products and services. Contact us today to experience the difference."
+                variants={fadeInVariants}
               />
-            </div>
+            </motion.div>
           </div>
 
           {/* Image container */}
