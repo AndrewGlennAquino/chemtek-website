f73da58c7f1ab2cd1f3eadc13f221a8b380ba69e
Andrew Aquino <andrewglenn.aquino@gmail.com>
2025-08-06 17:21:02 -0500

    Add comment

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,6 +4,9 @@ import WhyChooseUs from "../../components/why-choose-us/WhyChooseUs";
 import Testimonials from "../../components/testimonials/Testimonials";
 import Contact from "../../components/contact/Contact";
 
+/**
+ * Home page component with section components
+ */
 const Home = () => {
   return (
     <main>
