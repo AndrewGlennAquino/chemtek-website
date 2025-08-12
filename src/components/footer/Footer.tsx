a27dc6e68e016346765db7ac557931114bead6a7
Andrew Aquino <andrewglenn.aquino@gmail.com>
2025-08-12 17:33:01 -0500

    Add contacts flexbox

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,42 +6,58 @@ const Footer = () => {
   return (
     <footer className="bg-chemtek/50 text-smoke text-sm">
       {/* Footer container */}
-      <div className="container mp-default flex flex-col gap-4">
-        {/* Social media container */}
-        <div className="flex flex-col gap-2">
-          <h2 className="text-2xl font-bold flex">Socials</h2>
+      <div className="container mp-default flex flex-col gap-8">
+        {/* Social media and conctact flexbox */}
+        <div className="flex gap-8 md:gap-16">
+          {/* Social media container */}
+          <div className="flex flex-col gap-2">
+            <h2 className="text-2xl font-bold">Socials</h2>
 
-          <div className="flex gap-4">
-            {/* Facebook link */}
-            <motion.a
-              href="https://www.facebook.com/641706522363028"
-              target="_blank"
-              className="social-media-link"
-              whileHover={{ scale: 1.1 }}
-            >
-              <img
-                src={facebookIcon}
-                alt="Facebook logo"
-                className="w-full h-full"
-              />
-            </motion.a>
+            {/* Links flexbox */}
+            <div className="flex gap-4">
+              {/* Facebook link */}
+              <motion.a
+                href="https://www.facebook.com/641706522363028"
+                target="_blank"
+                className="social-media-link"
+                whileHover={{ scale: 1.1 }}
+              >
+                <img
+                  src={facebookIcon}
+                  alt="Facebook logo"
+                  className="w-full h-full"
+                />
+              </motion.a>
 
-            {/* LinkedIn link */}
-            <motion.a 
-              href="https://www.linkedin.com/company/chemtek-llc/"
-              target="_blank"
-              className="social-media-link"
-              whileHover={{ scale: 1.1 }}
-            >
-              <img
-                src={linkedinIcon}
-                alt="LinkedIn logo"
-                className="w-full h-full"
-              />
-            </motion.a>
+              {/* LinkedIn link */}
+              <motion.a
+                href="https://www.linkedin.com/company/chemtek-llc/"
+                target="_blank"
+                className="social-media-link"
+                whileHover={{ scale: 1.1 }}
+              >
+                <img
+                  src={linkedinIcon}
+                  alt="LinkedIn logo"
+                  className="w-full h-full"
+                />
+              </motion.a>
+            </div>
+          </div>
+
+          {/* Contact container */}
+          <div className="flex flex-col gap-2">
+            <h2 className="text-2xl font-bold">Contacts</h2>
+
+            {/* Contact flexbox */}
+            <div className="flex flex-col gap-1">
+              <a>(224) 659-0548</a>
+              <a>chemtekllc@gmail.com</a>
+            </div>
           </div>
         </div>
 
+        {/* Accredation to Flaticon */}
         <a href="https://www.flaticon.com/" title="Icons">
           Icons created by Freepik - Flaticon
         </a>
