9c0c541d8d72bdb2844f181e582dfd878d944dff
Andrew Aquino <andrewglenn.aquino@gmail.com>
2025-08-14 15:30:35 -0500

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Route, Routes } from "react-router";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
@@ -7,11 +6,7 @@ const App = () => {
   return (
     <>
       <Header />
-
-      <Routes>
-        <Route path="/chemtek-website" element={<Home />} />
-      </Routes>
-
+      <Home />
       <Footer />
     </>
   );
