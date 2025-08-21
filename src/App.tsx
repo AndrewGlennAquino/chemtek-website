4d478e09d3680c460ca513eda12e80af6fec8a9a
Andrew Aquino <andrew.aquino20@icloud.com>
2025-08-21 16:23:05 -0400

    Add first-time-specials page; Add react router routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
+import FirstTimeSpecials from "./pages/first-time-special/FirstTimeSpecial";
+import { Routes, Route } from "react-router";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/first-time-specials" element={<FirstTimeSpecials />} />
+      </Routes>
       <Footer />
     </>
   );
