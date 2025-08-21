086d4e9e12cd56915964e01b06f974c85dc43153
Andrew Aquino <andrew.aquino20@icloud.com>
2025-08-21 16:32:30 -0400

    Add firsttimespecials page

---
+++ src/pages/first-time-special/FirstTimeSpecial.tsx
@@ -0,0 +1,12 @@
+/**
+ * Page that can only be accessed via a promotional QR code
+ */
+const FirstTimeSpecials = () => {
+  return (
+    <main>
+      First Time Specials
+    </main>
+  );
+}
+
+export default FirstTimeSpecials;
