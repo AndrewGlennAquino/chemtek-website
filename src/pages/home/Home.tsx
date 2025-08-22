e24a6487b23b3384081d4d9eeef3ed0e21839af7
Andrew Aquino <andrew.aquino20@icloud.com>
2025-08-22 17:18:53 -0400

    Remove unused import

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,6 @@ import Hero from "../../components/hero/Hero";
 import WhatWeDo from "../../components/what-we-do/WhatWeDo";
 import WhyChooseUs from "../../components/why-choose-us/WhyChooseUs";
 import Testimonials from "../../components/testimonials/Testimonials";
-import Contact from "../../components/contact/Contact";
 import ServiceArea from "../../components/service-area/ServiceArea";
 import Equipment from "../../components/equipment/Equipment";
 
