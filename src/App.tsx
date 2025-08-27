984a5a518e42a832de18ebd16e59db7ff015dc5a
Andrew Aquino <andrew.aquino20@icloud.com>
2025-08-27 11:12:13 -0400

    Add page not found page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import FirstTimeCleaning from "./pages/first-time-cleaning/FirstTimeCleaning";
 import ChemicalProgram from "./pages/chemical-program/ChemicalProgram";
+import PageNotFound from "./pages/page-not-found/PageNotFound";
 import { Routes, Route } from "react-router";
 
 const App = () => {
@@ -14,6 +15,9 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/firsttimecleaning" element={<FirstTimeCleaning />} />
         <Route path="/chemicalprogram" element={<ChemicalProgram />} />
+
+        {/* Page not found for 404 */}
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Contact />
       <Footer />
---
+++ src/pages/page-not-found/PageNotFound.tsx
@@ -0,0 +1,12 @@
+/**
+ * Page not found page if there is a 404
+ */
+const PageNotFound = () => {
+  return (
+    <main className="text-4xl text-center h-screen flex justify-center items-center">
+      404 PAGE NOT FOUND {":("}
+    </main>
+  );
+}
+
+export default PageNotFound;
