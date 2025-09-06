07851b6d24a0c675bca8772d048784d43c4299f9
Andrew Aquino <andrewglenn.aquino@gmail.com>
2025-09-06 17:46:11 -0500

    Custom react hook that returns an array of all blogs posts

---
+++ src/hooks/useGetAllBlogs.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+
+// Types for blog object
+export type BlogObject = {
+  post_id: number;
+  post_title: string;
+  post_body: string;
+  created_at: Date;
+  updated_at: Date;
+};
+
+/**
+ * Custom hook that fetches all blogs from server, 
+ * then returns array of blog objects
+ */
+export const useGetAllBlogs = (): BlogObject[] => {
+  // Hold in state all blogs, initially an empty array
+  const [allBlogs, setAllBlogs] = useState<BlogObject[]>([]);
+
+  /**
+   * Once on mount, try fetching all blogs from Render PostgreSQL database.
+   * On successful fetch, setAllBlogs state to parsed response.
+   * On failure, console.log error and response status.
+   * On error, console.error message and error trace.
+   */
+  useEffect(() => {
+    const fetchAllBlogs = async () => {
+      try {
+        const response = await fetch("/.netlify/functions/getAllBlogs", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        // TODO: comment
+        if (response.ok) {
+          const data: BlogObject[] = await response.json();
+
+          setAllBlogs(data);
+
+          data.forEach((blog) => {
+            console.log(blog);
+          });
+        } else {
+          console.log("Unsuccessful GET request", response.status);
+        }
+      } catch (err) {
+        console.error("Unsuccessful GET request", err);
+      }
+    };
+
+    fetchAllBlogs();
+  }, []);
+
+  return allBlogs;
+};
