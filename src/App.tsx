31b5df8cb2ff7fb72f18cf57de98a33be3a3700f
Andrew Aquino <andrewglenn.aquino@gmail.com>
2025-09-08 16:54:22 -0500

    Wrap application with Auth0 provider

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,38 @@ import PageNotFound from "./pages/page-not-found/PageNotFound";
 import { Routes, Route } from "react-router";
 import Admin from "./pages/admin/Admin";
 import { Blogs } from "./pages/blogs/Blogs";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Routes>
-        {/* Landing page */}
-        <Route path="/" element={<Home />} />
+      <Auth0Provider
+        domain="dev-g473uzff0iy3v1fo.us.auth0.com"
+        clientId="TtvIjcf7sTRgc1V1kHXcuAsL3y8qkkah"
+        authorizationParams={{
+          // TODO: change url https://chemtek.services/chemtekkers
+          redirect_uri: "http://localhost:8888/chemtekkers",
+        }}
+      >
+        <Routes>
+          {/* Landing page */}
+          <Route path="/" element={<Home />} />
 
-        {/* Blogs page */}
-        <Route path="/blogs" element={<Blogs />} />
+          {/* Blogs page */}
+          <Route path="/blogs" element={<Blogs />} />
 
-        {/* Admin page with obscure endpoint to deter bots*/}
-        <Route path="/chemtekkers" element={<Admin />} />
+          {/* Admin page with obscure endpoint to deter bots*/}
+          <Route path="/chemtekkers" element={<Admin />} />
 
-        {/* Promotional pages */}
-        <Route path="/firsttimecleaning" element={<FirstTimeCleaning />} />
-        <Route path="/chemicalprogram" element={<ChemicalProgram />} />
+          {/* Promotional pages */}
+          <Route path="/firsttimecleaning" element={<FirstTimeCleaning />} />
+          <Route path="/chemicalprogram" element={<ChemicalProgram />} />
 
-        {/* Page not found for 404 */}
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+          {/* Page not found for 404 */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Auth0Provider>
       <Contact />
       <Footer />
     </>
