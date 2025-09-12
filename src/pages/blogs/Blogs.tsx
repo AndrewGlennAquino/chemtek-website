6d0fbd45d35e2c7162d2894cde6baa465adfee92
Andrew Aquino <andrewglenn.aquino@gmail.com>
2025-09-12 16:08:06 -0500

    Implement read more button

--- src/pages/blogs/Blogs.tsx
+++ src/pages/blogs/Blogs.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useGetAllBlogs } from "../../hooks/useGetAllBlogs";
 
 // Prop types for blogs
@@ -8,17 +9,77 @@ interface BlogProps {
 
 /**
  * Blog object that creates a blog card
- * TODO: add functionality to read more button
  * @param props title, body
  */
 const Blog = ({ title, body }: BlogProps) => {
+  // Hold in state if read more was clicked
+  const [readMore, setReadMore] = useState(false);
+
+  // Function that expands a popup window to read enitre body
+  const handleReadMore = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setReadMore(true);
+  };
+
+  // Function that closes popup window
+  const handleClose = (e?: React.MouseEvent<HTMLButtonElement>) => {
+    if (e) {
+      e.preventDefault();
+    }
+
+    setReadMore(false);
+  };
+
+  // /**
+  //  * On popup open, prevent background scrolling.
+  //  * On close, allow for background scrolling.
+  //  * Cleanup after unmount.
+  //  */
+  useEffect(() => {
+    if (readMore) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [readMore]);
+
   return (
     <div className="card-shadow p-4 rounded-md flex flex-col gap-2">
-      <h2>{title}</h2>
-      <p className="paragraph-shade">{body}</p>
-      <button className="gradient-border mr-auto cursor-pointer">
+      <h2 className="text-balance">{title}</h2>
+      <button
+        className="gradient-border mr-auto cursor-pointer"
+        onClick={handleReadMore}
+      >
         <div className="bg-night gradient-border-content">Read more</div>
       </button>
+
+      {/* Read more popup window */}
+      <div
+        className={`${
+          readMore ? `block` : `hidden`
+        } bg-night/95 w-full h-full max-h-screen absolute inset-0`}
+      >
+        {/* Read more content container */}
+        <div className="bg-night max-w-256 mp-default pt-24 w-full h-full overflow-y-auto">
+          {/* Blog title and close button container */}
+          <div className="flex justify-between items-center gap-2">
+            {/* Blog title */}
+            <h2 className="lg:text-2xl text-balance">{title}</h2>
+
+            {/* Close button */}
+            <button className="gradient-border ml-auto cursor-pointer" onClick={handleClose}>
+              <div className="bg-night gradient-border-content w-full h-full">CLOSE</div>
+            </button>
+          </div>
+
+          {/* Blog body */}
+          <p className="lg:text-lg paragraph-shade pt-4">{body}</p>
+        </div>
+      </div>
     </div>
   );
 };
