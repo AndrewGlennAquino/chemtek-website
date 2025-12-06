256fd01de3ad355cdc4cbd0223d823f0866017e1
Andrew Aquino <andrewglenn.aquino@gmail.com>
2025-12-06 17:58:26 -0600

    feat: optimize hamburger menu and header

---
+++ src/components/HamburgerMenu.tsx
@@ -0,0 +1,103 @@
+// Libraries
+import { useState, type Dispatch, type SetStateAction } from "react";
+import { motion } from "motion/react";
+
+/**
+ * Dropdown navigation menu that opens on HamburgerMenu button click
+ */
+const DropdownMenu = ({
+  setClicked,
+}: {
+  setClicked: Dispatch<SetStateAction<boolean>>;
+}) => {
+  return (
+    <nav className="bg-night w-full absolute top-16 left-0 right-0">
+      <div className="container mp-default text-right flex flex-col gap-4">
+        <a
+          href="/#what-we-do"
+          onClick={() => setClicked(false)}
+          className="dropdown-link"
+        >
+          What We Do
+        </a>
+        <a
+          href="/#why-choose-us"
+          onClick={() => setClicked(false)}
+          className="dropdown-link"
+        >
+          Why Choose Us
+        </a>
+        <a
+          href="/#service-area"
+          onClick={() => setClicked(false)}
+          className="dropdown-link"
+        >
+          Service Area
+        </a>
+        <a
+          href="/#equipment-fianance"
+          onClick={() => setClicked(false)}
+          className="dropdown-link"
+        >
+          Equipment Fianance
+        </a>
+        <a
+          href="/#"
+          onClick={() => setClicked(false)}
+          className="dropdown-link"
+        >
+          Testimonials
+        </a>
+        <a
+          href="/#contact"
+          onClick={() => setClicked(false)}
+          className="primary-button font-bold"
+        >
+          Contact Us
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+/**
+ * HamburgerMenu button that opens and closes DropdownMenu navigation
+ */
+export const HamburgerMenu = () => {
+  // Hold in state if button is clicked, default is false
+  const [clicked, setClicked] = useState(false);
+
+  return (
+    <>
+      <button
+        onClick={() => setClicked(!clicked)}
+        aria-label="Toggle dropdown menu"
+        className="xl:hidden"
+      >
+        <div className="flex flex-col gap-1">
+          <motion.div
+            className="hamburger-menu-bar"
+            animate={clicked ? "animateOpen" : undefined}
+            variants={{
+              animateOpen: { transform: "translateY(0.5rem) rotate(45deg)" },
+            }}
+          />
+          <motion.div
+            className="hamburger-menu-bar"
+            animate={clicked ? "animateOpen" : undefined}
+            variants={{ animateOpen: { opacity: 0 } }}
+          />
+          <motion.div
+            className="hamburger-menu-bar"
+            animate={clicked ? "animateOpen" : undefined}
+            variants={{
+              animateOpen: { transform: "translateY(-0.5rem) rotate(-45deg)" },
+            }}
+          />
+        </div>
+      </button>
+
+      {clicked && <DropdownMenu setClicked={setClicked} />}
+    </>
+  );
+};
---
+++ src/components/Header.tsx
@@ -0,0 +1,78 @@
+import { HamburgerMenu } from "./HamburgerMenu";
+import { motion } from "motion/react";
+import { Link } from "react-router";
+
+/**
+ * Header component that contains logo and responsive nav
+ */
+const Header = () => {
+  return (
+    <header className="bg-night header-shadow fixed sm:sticky top-0 left-0 right-0 z-50">
+      {/* Header container */}
+      <div className="container w-full h-16 mp-default py-4 flex justify-between items-center overflow-hidden">
+        {/* Header logo */}
+        <Link to="/">
+          <motion.div
+            className="logo relative flex justify-center items-center"
+            whileHover={{ scale: 1.1 }}
+          >
+            {/* Background glow */}
+            <div className="bg-aqua/20 blur-md w-32 h-32 absolute -z-10 rounded-full" />
+            <span>CHEMTEK</span>
+          </motion.div>
+        </Link>
+
+        {/* Hamburger menu only appears for device width less than 1280px */}
+        <HamburgerMenu />
+
+        {/* Links container for device width 1280px and greater */}
+        <nav className="hidden xl:flex justify-center items-center gap-8">
+          <motion.a
+            href="/#what-we-do"
+            className="dropdown-link"
+            whileHover={{ scale: 1.1 }}
+          >
+            What We Do
+          </motion.a>
+          <motion.a
+            href="/#why-choose-us"
+            className="dropdown-link"
+            whileHover={{ scale: 1.1 }}
+          >
+            Why Choose Us
+          </motion.a>
+          <motion.a
+            href="/#service-area"
+            className="dropdown-link"
+            whileHover={{ scale: 1.1 }}
+          >
+            Service Area
+          </motion.a>
+          <motion.a
+            href="/#equipment-fianance"
+            className="dropdown-link"
+            whileHover={{ scale: 1.1 }}
+          >
+            Equipment Fianance
+          </motion.a>
+          <motion.a
+            href="/#"
+            className="dropdown-link"
+            whileHover={{ scale: 1.1 }}
+          >
+            Testimonials
+          </motion.a>
+          <motion.a
+            href="/#contact"
+            className="primary-button font-bold"
+            whileHover={{ scale: 1.1 }}
+          >
+            Contact Us
+          </motion.a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
