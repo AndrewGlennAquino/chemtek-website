bac4b7ebe12fcd87f01bb64ff81d5496706e0bc0
Andrew Aquino <andrew.aquino20@icloud.com>
2026-07-15 17:05:17 -0400

    feat: implement header and dummy sections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
+import { Header } from "../components/Header";
+
 /**
  * Home page component that contains video hero, summary of services,
  */
 export const Home = () => {
   return (
     <>
-      UNDER CONSTRUCTION
+      <Header />
+      <div className="bg-white w-full h-screen" />
+      <div className="bg-red-500 w-full h-screen" />
     </>
   );
 };
